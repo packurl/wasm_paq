2f0fd3e51931413bddc4e3d5f97a10dafece654c
Jerome David <programingjd@gmail.com>
2022-11-14 22:24:32 +0100

    use wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use mashi_core::{Decoder, Encoder};
 // #[global_allocator]
 // static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 pub fn unpaq(data: &[u8]) -> Vec<u8> {
     let mut decoder = Decoder::new();
